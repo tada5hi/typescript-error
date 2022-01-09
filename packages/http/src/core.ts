8133a7c06a06d4328b827d35f728c9e648df3341
Peter <peter.placzek1996@gmail.com>
2022-01-09 09:29:36 +0100

    expose new utility functions from core module in http module

--- packages/http/src/core.ts
+++ packages/http/src/core.ts
@@ -1,11 +1,17 @@
 import {
     BaseError,
     ErrorOptions,
+    buildErrorOptions,
+    determineErrorMessage,
     mergeErrorOptions,
+    setUnsetErrorOptions,
 } from '@typescript-error/core';
 
 export {
     BaseError,
     ErrorOptions,
+    buildErrorOptions,
+    determineErrorMessage,
     mergeErrorOptions,
+    setUnsetErrorOptions,
 };
